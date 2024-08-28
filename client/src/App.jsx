cb703d31f5af5985769a6315f7ca86fc82b2e960
Bleza PLEGNON <bleza.plg@gmail.com>
2024-08-28 19:25:13 +0200

    Création d'un composant Footer.jsx pour y intégrer la logiquede filtrage précedemmentimplémentée dans le composant App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import TodoList from './components/TodoList';
 import AddTodoForm from './components/AddTodoForm';
+import Footer from './components/Footer';
 import { useTodos } from './hooks/useTodos';
 
 const App = () => {
@@ -85,44 +86,7 @@ const App = () => {
           onToggle={toggleTodo}
           onUpdate={updateTodo}
         />
-        <div className="mt-6 flex items-center space-x-20">
-          <span className="text-lg">
-          {/* Retourne le nombre de tâches restantes à accomplir */}
-            Tâches restantes: {todos.filter(todo => !todo.completed).length}
-          </span>
-          <div className="flex space-x-10">
-            <button
-              className={`px-4 py-2 rounded-full text-white font-semibold transition-colors durée-300 ${
-                filter === 'All'
-                  ? 'bg-gradient-to-r from-purple-400 to-pink-500 shadow-lg'
-                  : 'bg-gray-200 text-gray-600 hover:bg-gradient-to-r hover:from-purple-400 hover:to-pink-500 hover:text-white'
-              }`}
-              onClick={() => setFilter('All')}
-            >
-              Tout Voir 🎯
-            </button>
-            <button
-              className={`px-4 py-2 rounded-full text-white font-semibold transition-colors durée-300 ${
-                filter === 'Active'
-                  ? 'bg-gradient-to-r from-green-400 to-teal-500 shadow-lg'
-                  : 'bg-gray-200 text-gray-600 hover:bg-gradient-to-r hover:from-green-400 hover:to-teal-500 hover:text-white'
-              }`}
-              onClick={() => setFilter('Active')}
-            >
-              En Cours 🚀
-            </button>
-            <button
-              className={`px-4 py-2 rounded-full text-white font-semibold transition-colors durée-300 ${
-                filter === 'Completed'
-                  ? 'bg-gradient-to-r from-blue-400 to-indigo-500 shadow-lg'
-                  : 'bg-gray-200 text-gray-600 hover:bg-gradient-to-r hover:from-blue-400 hover:to-indigo-500 hover:text-white'
-              }`}
-              onClick={() => setFilter('Completed')}
-            >
-              Terminé 🎉
-            </button>
-          </div>
-        </div>
+       <Footer todos={todos} filter={filter} setFilter={setFilter} />
       </div>
     </div>
   );
