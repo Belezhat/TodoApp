0a5b68293f392a8c7164b6f9ea1c7efdb87a6366
Bleza PLEGNON <bleza.plg@gmail.com>
2024-08-29 17:11:13 +0200

    modifications

--- server/controllers/TodoController.js
+++ server/controllers/TodoController.js
@@ -39,7 +39,7 @@ exports.updateTodo = async (req, res) => {
             { new: true } // Renvoie la tâche mise à jour
         );
         if (!updatedTodo) {
-            return res.status(404).json({ error: 'Tâche non trouvée.' });
+            return res.status(400).json({ error: 'information érronné.' });
         }
         res.status(200).json(updatedTodo); 
     } catch (err) {
@@ -52,7 +52,7 @@ exports.deleteTodo = async (req, res) => {
     try {
         const deletedTodo = await Todo.findByIdAndDelete(req.params.id);
         if (!deletedTodo) {
-            return res.status(404).json({ error: 'Tâche non trouvée.' });
+            return res.status(400).json({ error: 'information érronnée.' });
         }
         res.status(200).json({ message: 'Tâche supprimée' }); 
     } catch (err) {
